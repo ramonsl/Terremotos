c8f339f91abde74b56c2417353fabaf4fc399c1d
ramonsl <ramonsl@gmail..com>
2018-04-29 19:34:24 -0300

    Adicionando a Classe EarthQuake

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,54 @@
+package com.example.android.quakereport;
+
+/**
+ * Created by ramonsl on 29/04/2018.
+ */
+
+public class Earthquake {
+
+    private double mMagnitude;
+
+
+    private String mLocation;
+    private long mTimeInMilliseconds;
+    private String mUrl;
+
+      public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
+        mMagnitude = magnitude;
+        mLocation = location;
+        mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
+    }
+
+    public double getmMagnitude() {
+        return mMagnitude;
+    }
+
+    public void setmMagnitude(double mMagnitude) {
+        this.mMagnitude = mMagnitude;
+    }
+
+    public String getmLocation() {
+        return mLocation;
+    }
+
+    public void setmLocation(String mLocation) {
+        this.mLocation = mLocation;
+    }
+
+    public long getmTimeInMilliseconds() {
+        return mTimeInMilliseconds;
+    }
+
+    public void setmTimeInMilliseconds(long mTimeInMilliseconds) {
+        this.mTimeInMilliseconds = mTimeInMilliseconds;
+    }
+
+    public String getmUrl() {
+        return mUrl;
+    }
+
+    public void setmUrl(String mUrl) {
+        this.mUrl = mUrl;
+    }
+}
